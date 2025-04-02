61679a6aa516c11dcde7c6e55bbf75e2cce3ce30
charlieweinberger <charlieweinberger05@gmail.com>
2025-04-01 22:27:31 -0700

    updated types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,21 @@
 import { Event } from "react-big-calendar";
 
+type CapitalLetter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
+type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+type dayOption = "M" | "Tu" | "W" | "Th" | "F";
+type TimeOption = 8 | 8.5 | 9 | 9.5 | 10 | 10.5 | 11 | 11.5 | 12 | 12.5 | 13 | 13.5 | 14 | 14.5 | 15 | 15.5 | 16 | 16.5 | 17 | 17.5 | 18 | 18.5 | 19 | 19.5 | 20 | 20.5 | 21 | 21.5 | 22;
+
 interface CourseEvent {
   name: string;
   code: number;
   type: "Lec" | "Dis" | "Lab";
-  section: string; // "A", "A1", "A2", "B", "B1", "B2", etc.
-  days: string[]; // combinations of "M", "T", "W", "Th", and "F"
-  startTime: number; // military time (ex: "17:00" for 5pm), 0.5 for half hours
-  endTime: number; // military time, 0.5 for half hours
+  section: `${CapitalLetter | ""}${Digit | ""}`;
+  days: dayOption[];
+  startTime: TimeOption;
+  endTime: TimeOption;
 }
 
 interface CalendarEvent extends Event {
-  id: number;
   color: string;
 }
